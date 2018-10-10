dbaf60519f2921e0e17d167dfacbfaef9dd06586
Aman Karn <aman.karn9@gmail.com>
2018-10-10 10:45:03 +0545

    Folder Renamed

---
+++ js-experiments/project/script.js
@@ -0,0 +1,28 @@
+let canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let map = [[]];
+
+
+
+function start(){
+
+
+
+}
+
+function update(){
+
+
+
+
+}
+
+
+function animate(){
+    update();
+    window.requestAnimationFrame(animate);
+}
+
+setup();
+animate();
